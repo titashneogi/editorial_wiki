0895bbbfbc0c538ee2837a3b80e81184e00eafb0
himanshu sharma <himanshu@himanshus-MacBook-Pro.local>
2016-07-07 18:41:51 +0530

    side Navigation added

--- script.js
+++ script.js
@@ -1,25 +1,29 @@
 	// create the module and name it Winnerz
-	var Editorial = angular.module('Editorial', ['ngRoute','ngStamplay','ui.bootstrap']);
+	var Editorial = angular.module('Editorial', ['ngRoute','ngStamplay','ui.bootstrap','wu.masonry']);
 
 	// configure our routes
 	Editorial.config(function($routeProvider) {
 		$routeProvider
-
 			// route for the home page
 			.when('/storylist/:id', {
 				templateUrl : 'pages/storylist.html',
 				controller  : 'mainController'
 			})
-			.when('/setup', {
+			.when('/setup/:id', {
 				templateUrl : 'pages/setup.html',
 				controller  : 'setupController'
 			})
 
-	});
+			.when('/allStories', {
+				templateUrl : 'pages/storylist.html',
+				controller  : 'allStoriesController'
+			})
+	})
 
 	// create the controller and inject Angular's $scope
 	Editorial.controller('mainController', function($scope, $rootScope,$routeParams, $http, $stamplay, $uibModal) {
 		var id = $routeParams.id;
+		$rootScope.ID = $routeParams.id;
 		Stamplay.Object("draft_story").get({page: 1, per_page: 100,username: id}).then(function(res) {
 			$scope.storylist = res.data;
 			$rootScope.list = res.data;
@@ -84,7 +88,7 @@
 		$scope.save =function(){
 			console.log($scope.data);
 			$http({
-			    url: 'http://localhost:3000/setup/',
+			    url: 'http://159.203.111.229:3000/setup/',
 			    dataType: 'json',
 			    method: 'POST',
 			    data: $scope.data,
@@ -99,4 +103,36 @@
 			    $scope.error = error;
 			});
 		}
+	});
+
+	Editorial.controller('allStoriesController', function($scope, $rootScope,$routeParams, $http, $stamplay, $uibModal) {
+		var id = $routeParams.id;
+		Stamplay.Object("draft_story").get({page: 1, per_page: 100}).then(function(res) {
+			$scope.storylist = res.data;
+			$rootScope.list = res.data;
+			$scope.$apply();
+		}, function(err) {
+			console.log(err);
+		})
+
+		$scope.animationsEnabled = true;
+		$scope.open = function (index) {
+			$rootScope.index = index;
+			var modalInstance = $uibModal.open({
+				animation: $scope.animationsEnabled,
+				templateUrl: 'myModalContent.html',
+				controller: 'ModalInstanceCtrl',
+				size: undefined
+			});
+
+			modalInstance.result.then(function (newstory) {
+				$scope.storylist[$rootScope.index].description = newstory.description
+			}, function () {
+				console.log("modal closed")
+			});
+		};
+
+		$scope.toggleAnimation = function () {
+			$scope.animationsEnabled = !$scope.animationsEnabled;
+		};
 	});
